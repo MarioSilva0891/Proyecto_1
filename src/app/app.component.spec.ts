d846d16494ad1049850734ba5b605ae35e3da4c4
MarioSilva0891 <mariosilvam1980@gamil.com>
2022-07-27 15:40:15 -0500

    Se hacen cambios en el modulo hello

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Proyecto_1'`, () => {
+  it(`should have as title 'DobleVia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Proyecto_1');
+    expect(app.title).toEqual('DobleVia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Proyecto_1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DobleVia app is running!');
   });
 });
